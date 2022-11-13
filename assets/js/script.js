2cf036e08a597c9e23cc56c41cce3644745dbbcf
Ivan Escobar <ivnescbr1989@gmail.com>
2022-11-13 16:46:52 -0500

    css file edit v1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,11 @@ var btn1 = document.getElementById(btn1)
 var btn2 = document.getElementById(btn2)
 var btn3 = document.getElementById(btn3)
 var btn4 = document.getElementById(btn4)
+var questions = [
+    {
 
+    }
+]
 
 
 function startQuiz(){
