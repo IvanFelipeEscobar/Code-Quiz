551db22365d3a55456cd43af42c7a1f55d9c58fd
Ivan Escobar <ivnescbr1989@gmail.com>
2022-11-13 16:37:52 -0500

    js file edit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var startBtn = document.getElementById(startBtn)
+var btn1 = document.getElementById(btn1)
+var btn2 = document.getElementById(btn2)
+var btn3 = document.getElementById(btn3)
+var btn4 = document.getElementById(btn4)
+
+
+
+function startQuiz(){
+
+}
+function submitAnswer(){
+
+}
